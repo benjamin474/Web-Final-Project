bab2d31751727416009c09ed7839f7bf9f2036ce
benjamin474 <coding47414@gmail.com>
2023-12-05 11:56:36 +0800

    小改

--- Stack.js
+++ Stack.js
@@ -1,31 +1,42 @@
-function push(){
+let Length = 3;
+
+function push() {
 
 }
 
-function pop(){
+function pop() {
 
 
 }
 
-function stackTop(){
+function stackTop() {
 
 }
 
-function assign(){ //出現輸入列 然後交給真正的build
+function assign() { //出現輸入列 然後交給真正的build
     let board = document.getElementById('input');
     let blank = document.createElement('input');
-    let submit = document.createElement('div');
-    
+    let submit = document.createElement('button');
+
     board.className = 'input';
     blank.type = Number;
     blank.placeholder = '請輸入0~10之間的數字';
 
+    submit.value = 'submit';
+    submit.addEventListener('click', build, false);
+
     board.appendChild(blank);
+    board.appendChild(submit);
 
 
-}   
+}
 
-function build(){
+function build() {
+    let getVal = document.getElementById('input');
+    Length = parseInt(getVal.value);
+    getVal.innerHTML = '';
+    
     let table = document.createElement('table');
+
 
 }
