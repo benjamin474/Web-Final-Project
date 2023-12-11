39bcd0f634a0ef90050549ec9fb8a0080f5798f1
benjamin474 <coding47414@gmail.com>
2023-12-11 23:39:16 +0800

    不小的變動
    Stack的Assign與Clear雛形

--- Stack.js
+++ Stack.js
@@ -1,4 +1,6 @@
 let Length = 3;
+let boardExist = false;
+
 
 function push() {
 
@@ -13,29 +15,47 @@ function stackTop() {
 
 }
 
-function assign() { //出現輸入列 然後交給真正的build
+function clearAll(){
+
+    
+    console.log('ClearAll function is activated.');
     let board = document.getElementById('input');
-    let blank = document.createElement('input');
-    let submit = document.createElement('button');
+    boardExist = false;
+    board.innerHTML = '';
+    board.setAttribute('background-color','transparent');
 
-    board.className = 'input';
-    blank.type = Number;
-    blank.placeholder = '請輸入0~10之間的數字';
+}
 
-    submit.value = 'submit';
-    submit.addEventListener('click', build, false);
+function assign() { //出現輸入列 然後交給真正的build
 
-    board.appendChild(blank);
-    board.appendChild(submit);
+    if (!boardExist) {
+        boardExist = true;
+        let board = document.getElementById('input');
+        let inputBoard = document.createElement('input');
+        let submit = document.createElement('button');
+        board.setAttribute('background-color','white');
+
+        board.className = 'input';
+        inputBoard.type = Number;
+        inputBoard.id = 'inputBoard';
+        inputBoard.placeholder = '請輸入0~10之間的數字';
+
+        submit.value = 'submit';
+        submit.addEventListener('click', build, false);
+
+        board.appendChild(inputBoard);
+        board.appendChild(submit);
+    }
 
 
 }
 
 function build() {
-    let getVal = document.getElementById('input');
+    let getVal = document.getElementById('inputBoard');
     Length = parseInt(getVal.value);
     getVal.innerHTML = '';
-    
+
+
     let table = document.createElement('table');
 
 
